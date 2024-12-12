2a1de4a15708ce5dc6dc09d2448de0f1910c7ae6
aymonier-elias <elias.aymonier@edu.univ-fcomte.fr>
2024-12-12 11:51:08 +0100

    Mise en forme plus script plus annimation de menu de filtre

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,16 @@
 const menuBtn = document.querySelector('.menu__btn');
 const nav = document.querySelector('.nav');
+const filtreBtn = document.querySelector('.btn__filter');
+const filtreNav = document.querySelector('.filter__nav');
 
 menuBtn.addEventListener("click", () =>{
     toggle(menuBtn, nav)
 });
+
+filtreBtn.addEventListener("click", () =>{
+    toggle(filtreBtn, filtreNav)
+});
+
 
 function toggle (btn, menu)  {
     console.log("Je fonctionne !")
